a5ccbe045db688f1d292dec292251eacb625e77a
Nathan VILLIERS <nathan.villiers@ens.uvsq.fr>
2021-10-13 18:48:30 +0200

    TD3 3 questions check

--- examples/hello/TD3àmoi.py
+++ examples/hello/TD3àmoi.py
@@ -1,3 +1,6 @@
+
+#convertit un temps en jour heure minute secondes en secondes
+
 def tempsEnSeconde(temps):
     jour=int(temps[0]*86400)
     heure=int(temps[1]*3600)
@@ -11,6 +14,8 @@ print(type(temps_jhms))
 print(tempsEnSeconde(temps_jhms))
 
 
+#convertit des secondes en jour heures minutes secondes
+
 def secondeEnTemps(tempssecondes):
     jours=int(tempssecondes//86400)
     heures=int((tempssecondes%86400)//3600)
@@ -23,7 +28,36 @@ print(temps[0],"jours\n",temps[1],"heures\n",temps[2],"minutes\n",temps[3],"seco
 
 
 
+#affichage d'un temps
 
+
+def afficheTemps(tempsobtenu):
+    if tempsobtenu[0]>1:
+        print(tempsobtenu[0],"jours",end=" ")
+    elif tempsobtenu[0]==1:
+        print(tempsobtenu[0],"jour",end=" ")
+
+    if tempsobtenu[1]>1:
+        print(tempsobtenu[1],"heures",end=" ")
+    elif tempsobtenu[1]==1:
+        print(tempsobtenu[1],"heure",end=" ")
+        
+
+    if tempsobtenu[2]>1:
+        print(tempsobtenu[2],"minutes",end=" ")
+    elif tempsobtenu[2]==1:
+        print(tempsobtenu[2],"minute",end=" ")
+    
+    
+    if tempsobtenu[3]>1:
+        print(tempsobtenu[3],"secondes",end=" ")
+    elif tempsobtenu[3]==1:
+        print(tempsobtenu[3],"seconde",end=" ")
+    
+    return tempsobtenu
+    
+tempsrecu=(1,0,14,23)
+print(afficheTemps(tempsrecu)) 
 
 
 
