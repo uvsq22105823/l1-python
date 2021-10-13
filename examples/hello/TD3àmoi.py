413d932843ef6abf6218c3520f2dc7ef755391be
Nathan VILLIERS <nathan.villiers@ens.uvsq.fr>
2021-10-13 18:12:57 +0200

    TD3 Premier exo comlpeté

---
+++ examples/hello/TD3àmoi.py
@@ -0,0 +1,29 @@
+def tempsEnSeconde(temps):
+    jour=int(temps[0]*86400)
+    heure=int(temps[1]*3600)
+    minute=int(temps[2]*60)
+    seconde=int(temps[3])
+    nb_secondes=jour+heure+minute+seconde
+    return nb_secondes
+
+temps_jhms = (3,23,1,34)
+print(type(temps_jhms))
+print(tempsEnSeconde(temps_jhms))
+
+
+def secondeEnTemps(tempssecondes):
+    jours=int(tempssecondes//86400)
+    heures=int((tempssecondes%86400)//3600)
+    minutes=int((tempssecondes%3600)//60)
+    secondes=int(tempssecondes%60)
+    return (jours,heures,minutes,secondes)
+
+temps = secondeEnTemps(100000)
+print(temps[0],"jours", temps[1],"heures", temps[2],"minutes", temps[3],"secondes")
+
+
+
+
+
+
+
