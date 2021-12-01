24edb20b590f0743ab9e2019718c6a06bb5adba8
Nathan VILLIERS <nathan.villiers@ens.uvsq.fr>
2021-12-01 17:34:22 +0100

    TD6 dégradé gris fini

--- exercises/TD05_gui/couleurs.py
+++ exercises/TD05_gui/couleurs.py
@@ -4,22 +4,42 @@ root.title("cible")
 root.geometry("400x256")
 canvas = tk.Canvas(root,width=256,height=256,bg="#000000")
 canvas.grid(row=1,column=3,rowspan=6,columnspan=6)
+import random as r
+
+def get_color(R, G, B):
+    """ Retourne une couleur à partir de ses composantes r, g, b entre 0 et 255"""
+    return '#{:02x}{:02x}{:02x}'.format(R, G, B)
 
 
-i=128
-j=129
 color="white"
 
 def draw_pixel(i,j,color):
-    canvas.create_line(i,i,j,j,fill=color)
+    canvas.create_rectangle(i,j,i,j,fill=color,outline="")
+
+def couleursaléatoires():
+    for i in range(256):
+        for j in range(256):
+            R=r.randint(0,255)
+            G=r.randint(0,255)
+            B=r.randint(0,255)
+            draw_pixel(i,j,color=get_color(R,G,B))
+        
+def dégradégris():
+    for i in range(256):
+        for j in range(256):
+            R=j
+            G=R
+            B=R
+            draw_pixel(j,i,color=get_color(R,G,B))
+
 
     
 
 
-bouton_aléatoire=tk.Button(root,text="Aléatoire",command=draw_pixel,relief="groove")
+bouton_aléatoire=tk.Button(root,text="Aléatoire",command=couleursaléatoires,relief="groove")
 bouton_aléatoire.grid(row=1,column=1)
 
-bouton_dégradégris=tk.Button(root,text="dégradé gris",command=root.destroy,relief="groove")
+bouton_dégradégris=tk.Button(root,text="dégradé gris",command=dégradégris,relief="groove")
 bouton_dégradégris.grid(row=3,column=1)
 
 bouton_dégradé2d=tk.Button(root,text="dégradé 2D",command=root.destroy,relief="groove")
@@ -29,9 +49,7 @@ bouton_dégradé2d.grid(row=5,column=1)
 
 
 
-def get_color(r, g, b):
-    """ Retourne une couleur à partir de ses composantes r, g, b entre 0 et 255"""
-    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
 
 
 
